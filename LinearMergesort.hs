79ef8c9663d66c38f964990ec684009050863b37
Chaitanya Koparkar <ckoparkar@gmail.com>
2021-05-02 15:14:43 -0400

    Use a custom Array type (currently identical to one in linear-base)

--- LinearMergesort.hs
+++ LinearMergesort.hs
@@ -12,24 +12,26 @@ import qualified Unsafe.Linear as Unsafe
 import Debug.Trace
 
 --------------------------------------------------------------------------------
+-- Sorting
+--------------------------------------------------------------------------------
 
-mergeSort :: forall a. Show a => a -> (a -> a -> Int) -> Arr.Array a %1-> Ur (Arr.Array a)
+mergeSort :: forall a. Show a => a -> (a -> a -> Int) -> Array2 a %1-> Ur (Array2 a)
 mergeSort init cmp src0 =
-    Arr.size src0 &
+    size2 src0 &
         \(Ur len, src1) ->
-            Arr.alloc len init (\tmp -> go src1 tmp)
+            alloc2 len init (\tmp -> go src1 tmp)
   where
-    go :: Show a => Arr.Array a %1-> Arr.Array a %1-> Ur (Arr.Array a)
+    go :: Show a => Array2 a %1-> Array2 a %1-> Ur (Array2 a)
     go src tmp =
         writeSort1 cmp src tmp &
             (Unsafe.toLinear Ur)
 
 -- | Sort the left and right halves of 'src' into 'tmp', and merge the results
 -- back into 'src'.
-writeSort1 :: Show a => (a -> a -> Int) -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+writeSort1 :: Show a => (a -> a -> Int) -> Array2 a %1-> Array2 a %1-> Array2 a
 writeSort1 cmp src0 tmp =
     {- INVARIANT: length src0 == length tmp -}
-    Arr.size src0 &
+    size2 src0 &
         \(Ur len, src) ->
             if len <= 1
             then tmp `lseq` src
@@ -47,10 +49,10 @@ writeSort1 cmp src0 tmp =
 
 -- | Sort the left and right halves of 'src' into 'tmp', and merge the results
 -- back into 'tmp'.
-writeSort2 :: Show a => (a -> a -> Int) -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+writeSort2 :: Show a => (a -> a -> Int) -> Array2 a %1-> Array2 a %1-> Array2 a
 writeSort2 cmp src0 tmp =
     {- INVARIANT: length src0 == length tmp -}
-    Arr.size src0 &
+    size2 src0 &
         \(Ur len, src) ->
             if len <= 1
             then tmp `lseq` src
@@ -68,7 +70,7 @@ writeSort2 cmp src0 tmp =
                                                  (src1 `lseq` writeMerge_par cmp n1 src_l1 n2 src_r1 tmp1)
 
 -- | Merge 'src_1' and 'src_2' into 'tmp'.
-writeMerge_par :: forall a. Show a => (a -> a -> Int) -> Int -> Arr.Array a %1-> Int -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+writeMerge_par :: forall a. Show a => (a -> a -> Int) -> Int -> Array2 a %1-> Int -> Array2 a %1-> Array2 a %1-> Array2 a
 writeMerge_par cmp n1 src_10 n2 src_20 tmp0 =
     if (n1+n2) <= 2
     then writeMerge_seq cmp n1 src_10 n2 src_20 tmp0
@@ -79,17 +81,17 @@ writeMerge_par cmp n1 src_10 n2 src_20 tmp0 =
              then src_20 `lseq` write_loop 0 0 n1 src_10 tmp0
              else go (n1 `div` 2) src_10 src_20 tmp0
   where
-    go :: Show a => Int -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+    go :: Show a => Int -> Array2 a %1-> Array2 a %1-> Array2 a %1-> Array2 a
     go mid1 src_1 src_2 tmp =
-        Arr.unsafeGet mid1 src_1 &
+        unsafeGet2 mid1 src_1 &
             \(Ur pivot, src_11) ->
                 binarySearch cmp pivot src_2 &
                     \(Ur mid2, src_21) ->
                         go2 mid1 mid2 pivot src_11 src_21 tmp
 
-    go2 :: Show a => Int -> Int -> a -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+    go2 :: Show a => Int -> Int -> a -> Array2 a %1-> Array2 a %1-> Array2 a %1-> Array2 a
     go2 mid1 mid2 pivot src_1 src_2 tmp00 =
-        Arr.unsafeSet (mid1+mid2) pivot tmp00 &
+        unsafeSet2 (mid1+mid2) pivot tmp00 &
             \tmp ->
                 {- (Unsafe.toLinear (traceShow (mid1,mid2,pivot))) (src_1 `lseq` src_2 `lseq` tmp) -}
                 splitAt2 mid1 (mid1+1) src_1 &
@@ -112,7 +114,7 @@ writeMerge_par cmp n1 src_10 n2 src_20 tmp0 =
 
 
 -- | Merge 'src_1' and 'src_2' into 'tmp'.
-writeMerge_seq :: Show a => (a -> a -> Int) -> Int -> Arr.Array a %1-> Int -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+writeMerge_seq :: Show a => (a -> a -> Int) -> Int -> Array2 a %1-> Int -> Array2 a %1-> Array2 a %1-> Array2 a
 writeMerge_seq cmp n1 src_1 n2 src_2 tmp = writeMerge_seq_loop 0 0 0 n1 n2 cmp src_1 src_2 tmp
 
 
@@ -120,26 +122,26 @@ writeMerge_seq cmp n1 src_1 n2 src_2 tmp = writeMerge_seq_loop 0 0 0 n1 n2 cmp s
 -- i1 index into src_1
 -- i2 index into src_2
 -- j index into tmp (output).
-writeMerge_seq_loop :: Show a => Int -> Int -> Int -> Int -> Int -> (a -> a -> Int) -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+writeMerge_seq_loop :: Show a => Int -> Int -> Int -> Int -> Int -> (a -> a -> Int) -> Array2 a %1-> Array2 a %1-> Array2 a %1-> Array2 a
 writeMerge_seq_loop i1 i2 j n1 n2 cmp src_1 src_2 tmp =
     if (i1 == n1)
     then src_1 `lseq` write_loop_seq j i2 n2 src_2 tmp
     else if i2 == n2
          then src_2 `lseq` write_loop_seq j i1 n1 src_1 tmp
          else
-             Arr.unsafeGet i1 src_1 &
+             unsafeGet2 i1 src_1 &
                  \(Ur x1, src_11) ->
-                     Arr.unsafeGet i2 src_2 &
+                     unsafeGet2 i2 src_2 &
                          \(Ur x2, src_21) ->
                              if cmp x1 x2 < 0
                              then
-                                 Arr.unsafeSet j x1 tmp &
+                                 unsafeSet2 j x1 tmp &
                                      \tmp_1 -> writeMerge_seq_loop (i1+1) i2 (j+1) n1 n2 cmp src_11 src_21 tmp_1
                              else
-                                 Arr.unsafeSet j x2 tmp &
+                                 unsafeSet2 j x2 tmp &
                                      \tmp_1 -> writeMerge_seq_loop i1 (i2+1) (j+1) n1 n2 cmp src_11 src_21 tmp_1
 
-write_loop :: Show a => Int -> Int -> Int -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+write_loop :: Show a => Int -> Int -> Int -> Array2 a %1-> Array2 a %1-> Array2 a
 write_loop = write_loop_seq
 
 -- | Copy elements from 'from' to 'to'.
@@ -147,14 +149,14 @@ write_loop = write_loop_seq
 -- from[from_idx]    ===>   to[to_idx]
 -- from[from_idx+1]  ===>   to[to_idx+1]
 -- ...
-write_loop_seq :: Show a => Int -> Int -> Int -> Arr.Array a %1-> Arr.Array a %1-> Arr.Array a
+write_loop_seq :: Show a => Int -> Int -> Int -> Array2 a %1-> Array2 a %1-> Array2 a
 write_loop_seq to_idx from_idx end from to =
     if from_idx == end
     then from `lseq` to
     else
-        Arr.unsafeGet from_idx from &
+        unsafeGet2 from_idx from &
             \(Ur val, from1) ->
-                Arr.unsafeSet to_idx val to &
+                unsafeSet2 to_idx val to &
                     \to1 -> write_loop_seq (to_idx+1) (from_idx+1) end from1 to1
 
 -- | Return 'query's *position* in 'vec'.
@@ -162,20 +164,20 @@ write_loop_seq to_idx from_idx end from to =
 -- That is, return a *p* s.t.
 -- (1) elements vec[0]..vec[p] are less than query, and
 -- (2) elements vec[p+1]..vec[end] are greater than query.
-binarySearch :: forall a. Show a => (a -> a -> Int) -> a -> Arr.Array a %1-> (Ur Int, Arr.Array a)
+binarySearch :: forall a. Show a => (a -> a -> Int) -> a -> Array2 a %1-> (Ur Int, Array2 a)
 {-# INLINE binarySearch #-}
 binarySearch cmp query = Unsafe.toLinear go
   where
-    go :: Show a => Arr.Array a -> (Ur Int, Arr.Array a)
-    go vec = let (Ur len, vec1) = Arr.size vec
+    go :: Show a => Array2 a -> (Ur Int, Array2 a)
+    go vec = let (Ur len, vec1) = size2 vec
              in (Ur (binarySearch' 0 len cmp vec1 query), vec1)
 
-binarySearch' :: Show a => Int -> Int -> (a -> a -> Int) -> Arr.Array a %1-> a -> Int
+binarySearch' :: Show a => Int -> Int -> (a -> a -> Int) -> Array2 a %1-> a -> Int
 binarySearch' lo hi cmp vec query =
     if n == 0
     then vec `lseq` lo
     {- mid = (lo+(div n 2)) -}
-    else  Arr.unsafeGet (lo+(div n 2)) vec &
+    else  unsafeGet2 (lo+(div n 2)) vec &
           \(Ur pivot, vec1) ->
               if (cmp query pivot) < 0
               then binarySearch' lo (lo+(div n 2)) cmp vec1 query
@@ -185,22 +187,10 @@ binarySearch' lo hi cmp vec query =
   where
     n = hi - lo
 
+
 --------------------------------------------------------------------------------
-
--- | 'Arr.slice' is not O(1), it copies elements. So this splitAt is quite expensive :(
-splitAt :: Show a => Int -> Arr.Array a %1-> ((Ur Int,Arr.Array a),(Ur Int,Arr.Array a),Arr.Array a)
-splitAt n arr0 = splitAt2 n n arr0
-
-splitAt2 :: Show a => Int -> Int -> Arr.Array a %1-> ((Ur Int,Arr.Array a),(Ur Int,Arr.Array a),Arr.Array a)
-splitAt2 n m arr0 =
-    Arr.size arr0 &
-        \(Ur len, arr1) ->
-            Arr.slice 0 n arr1 &
-                \(arr2, slice1) ->
-                    Arr.slice m (len - m) arr2 &
-                        \(arr3, slice2) ->
-                            ((Ur n,slice1), (Ur (len-n),slice2), arr3)
-
+-- Tests
+--------------------------------------------------------------------------------
 
 goto_seqmerge :: Int
 {-# INLINE goto_seqmerge #-}
@@ -209,9 +199,6 @@ goto_seqmerge = 4096
 spawn :: Show a => a -> a
 {-# INLINE spawn #-}
 spawn x = x
-
---------------------------------------------------------------------------------
--- Tests
 
 compare1 :: Show a => Ord a => a -> a -> Int
 compare1 r1 r2 =
@@ -222,11 +209,60 @@ compare1 r1 r2 =
 
 sortList :: [Int] -> [Int]
 sortList xs =
-    Arr.fromList xs (\input -> mergeSort 0 compare1 input) &
-        \(Ur sorted) -> Unsafe.toLinear unur (Arr.toList sorted)
+    fromList2 xs (\input -> mergeSort 0 compare1 input) &
+        \(Ur sorted) -> Unsafe.toLinear unur (toList2 sorted)
 
 genRevList :: Int -> [Int]
 genRevList n = [n,n-1..1]
 
 test1 :: Bool
 test1 = sortList (genRevList 10) == [1..10]
+
+
+--------------------------------------------------------------------------------
+-- An Arrays API
+--------------------------------------------------------------------------------
+
+{-
+ - See Ed Kmett's comments here:
+ -
+ - [1]: https://github.com/tweag/linear-base/issues/312
+ - [2]: https://github.com/ekmett/linear-haskell/blob/6f9e5c0e96d0c99d064ae027086db48c4fcfc63c/linear-keys/src/Linear/Range.hs
+ -}
+
+type Array2 a = Arr.Array a
+
+size2 :: Array2 a %1 -> (Ur Int, Array2 a)
+size2 = Arr.size
+
+slice2 :: Int -> Int -> Array2 a %1-> (Array2 a, Array2 a)
+slice2 = Arr.slice
+
+fromList2 :: [a] -> (Array2 a %1-> Ur b) %1 -> Ur b
+fromList2 = Arr.fromList
+
+toList2 :: Array2 a %1-> Ur [a]
+toList2 = Arr.toList
+
+alloc2 :: Int -> a -> (Array2 a %1-> Ur b) %1 -> Ur b
+alloc2 = Arr.alloc
+
+unsafeGet2 :: Int -> Array2 a %1-> (Ur a, Array2 a)
+unsafeGet2 = Arr.unsafeGet
+
+unsafeSet2 :: Int -> a -> Array2 a %1-> Array2 a
+unsafeSet2 = Arr.unsafeSet
+
+-- | 'slice2' is not O(1), it copies elements. So this splitAt is quite expensive :(
+splitAt :: Show a => Int -> Array2 a %1-> ((Ur Int,Array2 a),(Ur Int,Array2 a),Array2 a)
+splitAt n arr0 = splitAt2 n n arr0
+
+splitAt2 :: Show a => Int -> Int -> Array2 a %1-> ((Ur Int,Array2 a),(Ur Int,Array2 a),Array2 a)
+splitAt2 n m arr0 =
+    size2 arr0 &
+        \(Ur len, arr1) ->
+            slice2 0 n arr1 &
+                \(arr2, slice1) ->
+                    slice2 m (len - m) arr2 &
+                        \(arr3, slice2) ->
+                            ((Ur n,slice1), (Ur (len-n),slice2), arr3)
